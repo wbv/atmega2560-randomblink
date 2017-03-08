bd8162bf26e2f1ae0ab82cda95053eeb6ec4e29b
Walter B. Vaughan <wbv@wbv-desktop.local>
2017-03-07 18:58:21 -0600

    modified to use slower clock speed (save power)

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <avr/io.h>
+#include <avr/power.h>
 #include <util/delay_basic.h>
 #include <stdlib.h>
 
@@ -6,20 +7,23 @@ int main()
 {
 	uint8_t mask = 0x80;
 	uint16_t delay;
-	uint8_t i;
 
 	/* on atmega2560, PWM pin 13 (LED pin) = port B bit 7 */
 	DDRB |= mask; /* set port B pin 7 to output */
 
+	/* slow this jam w a a y   d o w n */
+	/* (arduino clock down to 16/128 MHz = 125 kHz) */
+	clock_prescale_set(clock_div_128);
+
 	while (1)
 	{
 		delay = rand();
-		PORTB |= mask; /* set port B pin 7 to high */
-		for (i = 0; i < 0x40; i++) _delay_loop_2(0x1000);
-		PORTB &= ~mask; /* set port B pin 7 to low */
-		for (i = 0; i < 0x40; i++) _delay_loop_2(delay);
-	}
 
+		PORTB |= mask; /* set port B pin 7 to high */
+		_delay_loop_2(0x200);
+		PORTB &= ~mask; /* set port B pin 7 to low */
+		_delay_loop_2(delay);
+	}
 
 	return 0;
 }
